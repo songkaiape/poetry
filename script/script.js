c9f2542caa87ca6eafd92389dd1f51229346f422
songkaiape <songkaiape@gmail.com>
2018-11-10 13:57:44 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -4,7 +4,7 @@ $('.name-overlay .input-name').focus();
 
 $('.name-overlay .input-name').keypress(function(event) {
 	if(event.which == 13) {
-		if($('.name-overlay .input-name').val() != 'xxx') {
+		if($('.name-overlay .input-name').val() != '阿惠') {
 			$('.name-overlay .warning').css('color', 'red');
 			return;
 		}
